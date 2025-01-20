2e7dbe1b502de1af827919c366635aa957871c80
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2025-01-20 12:09:16 -0300

    funciona con modulos

--- build.zig
+++ build.zig
@@ -83,10 +83,13 @@ pub fn build(b: *std.Build) void {
     exe.root_module.addImport("dotenv", dotenv_mod);
     const deserialize = b.addModule("deserialize", .{ .root_source_file = .{ .cwd_relative = "src/deserialize.zig" } });
     exe.root_module.addImport("deserialize", deserialize);
-
     // This creates a build step. It will be visible in the `zig build --help` menu,
     // and can be selected like this: `zig build run`
     // This will evaluate the `run` step rather than the default, which is "install".
+    const spotify_token = b.addModule("spotify-token", .{ .root_source_file = .{ .cwd_relative = "src/spotify_token.zig" } });
+    exe.root_module.addImport("spotify-token", spotify_token);
+    spotify_token.addImport("dotenv", dotenv_mod);
+
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
@@ -96,12 +99,13 @@ pub fn build(b: *std.Build) void {
     const exe_unit_tests = b.addTest(.{
         .root_module = exe_mod,
     });
-
+    exe_unit_tests.root_module.addImport("dotenv", dotenv_mod);
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
+
     test_step.dependOn(&run_exe_unit_tests.step);
 }
---
+++ src/spotify_token.zig
@@ -0,0 +1,132 @@
+const std = @import("std");
+const InnerToken = struct { token: []const u8, expiration_timestamp: i64 };
+const dumpfile = "dump.a";
+const TokenTuple = std.meta.Tuple(&.{ []u8, i32 });
+const dotenv = @import("dotenv");
+const http = std.http;
+
+const SpotifyResponse = struct {
+    access_token: []u8,
+    token_type: []u8,
+    expires_in: i32,
+    pub fn format(
+        self: @This(),
+        comptime fmt: []const u8,
+        _: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        if (fmt.len != 0) {
+            std.fmt.invalidFmtError(fmt, self);
+        }
+        return writer.print("{s}\naccess_token: {s}\ntoken_type: {s}\nexpires_in: {d}\n", .{ @typeName(@TypeOf(self)), self.access_token, self.token_type, self.expires_in });
+    }
+};
+
+pub const SerializedToken = struct {
+    expiration_timestamp: i64,
+    token: []const u8,
+    arena: std.mem.Allocator,
+    pub fn init(allocator: std.mem.Allocator) !SerializedToken {
+        const filename = dumpfile;
+        const cwd = std.fs.cwd();
+        const file = cwd.openFile(filename, .{});
+        const token, const expiration_timestamp = blk: {
+            if (file) |value| {
+                std.debug.print("Token file already exists\n", .{});
+                const str = try value.readToEndAlloc(allocator, 4096);
+                const token_data = try std.json.parseFromSlice(InnerToken, allocator, str, .{ .ignore_unknown_fields = true, .parse_numbers = true });
+                defer value.close(); // Cierra el archivo abierto, dentro de este scope.
+                std.debug.print("Using token: {s}\n", .{token_data.value.token});
+                break :blk .{ token_data.value.token, token_data.value.expiration_timestamp };
+            } else |err| {
+                switch (err) {
+                    std.fs.File.OpenError.FileNotFound => {
+                        std.debug.print("Token file does not exist\n", .{});
+                        const token, const void_timestamp = try get_token(allocator);
+                        const expiration_timestamp = void_timestamp + @divTrunc(std.time.milliTimestamp(), 1000);
+                        const dump_data = InnerToken{ .expiration_timestamp = expiration_timestamp, .token = token };
+                        const dumpu8 = try std.json.stringifyAlloc(allocator, dump_data, .{});
+                        const filew = try std.fs.cwd().createFile(dumpfile, .{ .read = true });
+                        try filew.writeAll(dumpu8);
+                        defer filew.close();
+                        break :blk .{ token, expiration_timestamp };
+                    },
+
+                    else => {
+                        std.debug.print("El error es aca", .{});
+                        return err;
+                    },
+                }
+            }
+        };
+
+        const ret = SerializedToken{ .expiration_timestamp = expiration_timestamp, .token = token, .arena = allocator };
+        return ret;
+    }
+    fn update(self: *SerializedToken) !void {
+        const token, const void_timestamp = try get_token(self.arena);
+        const expiration_timestamp = void_timestamp + @divTrunc(std.time.milliTimestamp(), 1000);
+        self.expiration_timestamp = expiration_timestamp;
+        self.token = token;
+        const dump_data = InnerToken{ .expiration_timestamp = expiration_timestamp, .token = token };
+        const dumpu8 = try std.json.stringifyAlloc(self.arena, dump_data, .{});
+        const file = try std.fs.cwd().openFile(dumpfile, .{ .mode = .read_write });
+        try file.writeAll(dumpu8);
+        defer file.close();
+    }
+    pub fn retrieve(self: *SerializedToken) ![]const u8 {
+        const current_timestamp = @divTrunc(std.time.milliTimestamp(), 1000);
+        if (current_timestamp > self.expiration_timestamp) {
+            try self.update();
+        }
+        return self.token;
+    }
+};
+fn get_token(allo: std.mem.Allocator) !TokenTuple {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var local_arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer local_arena.deinit();
+    const client_id, const client_secret = try get_dotenv(local_arena.allocator());
+    var client = http.Client{ .allocator = local_arena.allocator() };
+    const url = try std.Uri.parse("https://accounts.spotify.com/api/token");
+    var buf: [4096]u8 = undefined;
+    const headers = http.Client.Request.Headers{ .content_type = .{ .override = "application/x-www-form-urlencoded" } };
+    const body = try std.fmt.allocPrint(local_arena.allocator(), "grant_type=client_credentials&client_id={s}&client_secret={s}", .{ client_id, client_secret });
+    var response = std.ArrayList(u8).init(local_arena.allocator());
+    const options = http.Client.FetchOptions{ .response_storage = .{ .dynamic = &response }, .method = .POST, .payload = body, .server_header_buffer = &buf, .headers = headers, .location = http.Client.FetchOptions.Location{ .uri = url } };
+    _ = try client.fetch(options);
+    std.debug.print("\n{s}\n", .{response.items});
+    const obj = try std.json.parseFromSlice(SpotifyResponse, allo, response.items, .{ .ignore_unknown_fields = true });
+    const token = obj.value.access_token;
+    const expiration = obj.value.expires_in;
+    // std.debug.print("Token expires in = {}", .{obj.value.expires_in});
+    const ret: TokenTuple = .{ token, expiration };
+    return ret;
+}
+
+fn get_dotenv(allocator: std.mem.Allocator) ![2][]const u8 {
+    var envs = try dotenv.getDataFrom(allocator, ".env");
+    const client_id = envs.get("client_id").?.?;
+    const client_secret = envs.get("client_secret").?.?;
+    return .{ client_id, client_secret };
+}
+test "holds token" {
+    const testing = std.testing.allocator;
+    var local_arena = std.heap.ArenaAllocator.init(testing);
+    defer local_arena.deinit();
+    var token_wrapper = try SerializedToken.init(local_arena.allocator());
+    const token1 = try token_wrapper.retrieve();
+    var token_wrapper2 = try SerializedToken.init(local_arena.allocator());
+    const token2 = try token_wrapper2.retrieve();
+    std.debug.print("t1{s}\nt2{s}\n", .{ token1, token2 });
+    try std.testing.expectEqualStrings(token1, token2);
+}
+test "gets token" {
+    const testing = std.testing.allocator;
+    var local_arena = std.heap.ArenaAllocator.init(testing);
+    defer local_arena.deinit();
+    var token_wrapper = try SerializedToken.init(local_arena.allocator());
+    const token = try token_wrapper.retrieve();
+    std.debug.print("{s}", .{token});
+    try std.testing.expect(true);
+}
